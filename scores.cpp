9d693cd2597bbf709d38a09946b0ad0a28b6ef7d
Zeno Rogue <zeno@attnam.com>
2018-04-14 09:43:42 +0200

    fixed score sorting

--- scores.cpp
+++ scores.cpp
@@ -36,7 +36,9 @@ int modediff(score *S) {
   if(S->box[238]) g = gSphere;
   if(S->box[239]) g = gElliptic;
   if(max(S->box[197], 1) != multi::players) diff += 8;
-  if(S->box[186] != nonbitrunc || S->box[341] != gp::on || S->box[342] != gp::param.first || S->box[343] != gp::param.second) 
+  if(S->box[186] != nonbitrunc || S->box[341] != gp::on)
+    diff += 16;
+  if(S->box[341] && gp::on && (S->box[342] != gp::param.first || S->box[343] != gp::param.second))
     diff += 16;
   if(S->box[196] != chaosmode) diff += 32;
   if(S->box[119] != shmup::on) diff += 64;
