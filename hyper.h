09c2a2096bb3a4988f645710410c4bb9c12e5576
Zeno Rogue <zeno@attnam.com>
2018-06-18 17:54:38 +0200

    10.4d

--- hyper.h
+++ hyper.h
@@ -2,9 +2,9 @@
 // It is quite chaotic.
 
 // version numbers
-#define VER "10.4c"
-#define VERNUM 10403
-#define VERNUM_HEX 0xA0B3
+#define VER "10.4d"
+#define VERNUM 10404
+#define VERNUM_HEX 0xA0B4
 
 #include <stdarg.h>
 
