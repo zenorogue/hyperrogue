6d8fe2bb2550c18fabf4a3cdcbc1f2db4837be92
Zeno Rogue <zeno@attnam.com>
2018-07-11 12:57:13 +0200

    'expanded' now returned in operation_name

--- goldberg.cpp
+++ goldberg.cpp
@@ -638,6 +638,8 @@ namespace hr { namespace gp {
       return XLAT("OFF");
     else if(param == loc(1, 1) && S3 == 3)
       return XLAT("bitruncated");
+    else if(param == loc(1, 1) && S3 == 4)
+      return XLAT("expanded");
     else if(param == loc(2, 0))
       return S3 == 3 ? XLAT("chamfered") : XLAT("expanded");
     else if(param == loc(3, 0) && S3 == 3)
