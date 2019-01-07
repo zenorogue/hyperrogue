f3470a21fde0e59dc0a08c87727d306abfe2ad90
Zeno Rogue <zeno@attnam.com>
2019-01-07 04:56:20 +0100

    updated version in hyperstone achievements

--- achievement.cpp
+++ achievement.cpp
@@ -11,7 +11,7 @@ const char* leadernames[NUMLEADER] = {
   "Score", "Diamonds", "Gold", "Spice", "Rubies", "Elixirs",
   "Shards100", "Totems", "Daisies", "Statues", "Feathers", "Sapphires",
   "Hyperstones", "Time to Win-71", "Turns to Win-71",
-  "Time to 10 Hyperstones-103", "Turns to 10 Hyperstones-103", "Orbs of Yendor",
+  "Time to 10 Hyperstones-110", "Turns to 10 Hyperstones-110", "Orbs of Yendor",
   "Fern Flowers", 
   "Royal Jellies", "Powerstones", "Silver", "Wine", "Emeralds", "Grimoires",
   "Holy Grails", "Red Gems", "Pirate Treasures",
