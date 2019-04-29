cf4829634f5f19c4125f5e5562d73d22a74fc02f
Zeno Rogue <zeno@attnam.com>
2019-04-29 03:38:47 +0200

    quitsaves() depends on CAP_SAVE, not havesave

--- quit.cpp
+++ quit.cpp
@@ -3,7 +3,7 @@
 
 namespace hr {
 
-bool quitsaves() { return (items[itOrbSafety] && havesave && !archimedean); }
+bool quitsaves() { return (items[itOrbSafety] && CAP_SAVE && !archimedean); }
 
 bool needConfirmationEvenIfSaved() {
   return canmove && (gold() >= 30 || tkills() >= 50) && !cheater;
