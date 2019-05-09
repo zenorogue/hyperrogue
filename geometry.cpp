d3b2d7f37174d67283b6f02c2f1b035ef55951d0
Zeno Rogue <zeno@attnam.com>
2019-05-10 01:01:05 +0200

    2d3d:: fixed INFDEEP on sphere

--- geometry.cpp
+++ geometry.cpp
@@ -335,7 +335,7 @@ namespace geom3 {
       BIRD = 1.20;
       }
     else {
-      INFDEEP = GDIM == 3 ? +10 : (euclid || sphere) ? 0.01 : lev_to_projection(0) * tanh(camera);
+      INFDEEP = GDIM == 3 ? (sphere ? M_PI/2 : +10) : (euclid || sphere) ? 0.01 : lev_to_projection(0) * tanh(camera);
       ld wh = actual_wall_height();
       WALL = lev_to_factor(wh);
       FLOOR = lev_to_factor(0);
