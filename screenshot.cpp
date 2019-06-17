ecfaa7c49dab228b0292587a6f4c57d3882a5a5f
Zeno Rogue <zeno@attnam.com>
2019-06-17 11:00:40 +0200

    ascii: no more animations in low graphics

--- screenshot.cpp
+++ screenshot.cpp
@@ -1216,7 +1216,7 @@ void fib_ghosts() {
 reaction_t current = null_animation;
 
 void pick() {
-  if(((gold() > 0 || tkills() > 0) && canmove) || geometry != gNormal || ISWEB || ISMOBILE || vid.always3 || pmodel || rug::rugged) {
+  if(((gold() > 0 || tkills() > 0) && canmove) || geometry != gNormal || ISWEB || ISMOBILE || vid.always3 || pmodel || rug::rugged || vid.wallmode < 2 || vid.monmode < 2) {
     current = null_animation;
     return;
     }
