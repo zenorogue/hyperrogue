a4418d6dcccd8615ffa1623601ea8442ed8fd8b5
Zeno Rogue <zeno@attnam.com>
2019-09-28 17:33:59 +0200

    11.2a

--- hyper.h
+++ hyper.h
@@ -13,8 +13,8 @@
 #define _HYPER_H_
 
 // version numbers
-#define VER "11.2"
-#define VERNUM_HEX 0xA800
+#define VER "11.2a"
+#define VERNUM_HEX 0xA801
 
 #include "sysconfig.h"
 #include "classes.h"
