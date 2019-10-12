8ebeb60bcb4d928307c0e25dacd94c7d2e405171
Zeno Rogue <zeno@attnam.com>
2019-10-12 13:13:09 +0200

    increased default vid.texture_step in paracompact honeycombs

--- system.cpp
+++ system.cpp
@@ -1220,6 +1220,7 @@ EX void set_geometry(eGeometry target) {
     if(was_default) pmodel = default_model();
     if(nonisotropic && old_DIM == 2 && vid.texture_step < 4) vid.texture_step = 4;
     if(prod) { pmodel = mdPerspective; if(vid.texture_step < 4) vid.texture_step = 4; }
+    if(WDIM == 3 && (cgflags & qIDEAL) && vid.texture_step < 4) vid.texture_step = 4;
     if(sl2) nisot::geodesic_movement = true;
     }
   }
