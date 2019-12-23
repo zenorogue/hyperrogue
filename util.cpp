a83220c34e0ef277c2d1b0ff056e7a09b5e2d6c7
Zeno Rogue <zeno@attnam.com>
2019-12-23 21:18:45 +0100

    parser:: 'deg' as a default constant

--- util.cpp
+++ util.cpp
@@ -255,6 +255,7 @@ cld exp_parser::parse(int prio) {
     else if(number == "ms") res = ticks;
     else if(number[0] == '0' && number[1] == 'x') res = strtoll(number.c_str()+2, NULL, 16);
     else if(number == "mousex") res = mousex;
+    else if(number == "deg") res = degree;
     else if(number == "mousey") res = mousey;
     else if(number == "random") res = randd();
     else if(number == "mousez") res = cld(mousex - current_display->xcenter, mousey - current_display->ycenter) / cld(current_display->radius, 0);
