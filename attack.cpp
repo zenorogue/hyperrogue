5cdafd44887a639eaeed880147a8dc1a7418e5d1
Zeno Rogue <zeno@attnam.com>
2019-12-25 23:03:46 +0100

    ivy_total fix

--- attack.cpp
+++ attack.cpp
@@ -470,7 +470,7 @@ EX void killMonster(cell *c, eMonster who, flagtype deathflags IS(0)) {
     if(isMutantIvy(m)) clearing::direct++;
     bignum s = ivy_total() - 1;
     killMutantIvy(c, who);
-    s = ivy_total();
+    s = ivy_total() - s;
     if(s > bignum(1))
       drawBubble(c, 0xFFFF00, s.get_str(100), .5);
     }
