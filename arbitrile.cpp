13c1d85439b2b7625fdeee1b1b09bea7011e3b11
Zeno Rogue <zeno@attnam.com>
2020-01-15 17:55:04 +0100

    arbi:: check errors

--- arbitrile.cpp
+++ arbitrile.cpp
@@ -396,6 +396,12 @@ struct hrmap_arbi : hrmap {
     for(auto& p2: altmap[alt]) if(id_of(p2.first) == xt && hdist(tC0(p2.second), tC0(T)) < 1e-2) {
       for(int oth=0; oth < p2.first->type; oth++)
         if(hdist(p2.second * xsh.vertices[oth], T * xsh.vertices[e]) < 1e-2) {
+          ld err = hdist(p2.second * xsh.vertices[oth], T * xsh.vertices[e]);
+          static ld max_err = 0;
+          if(err > max_err) {
+            println(hlog, "err = ", err);
+            max_err = err;
+            }
           h->c.connect(d, p2.first, oth%p2.first->type, m);
           return p2.first;
           }
