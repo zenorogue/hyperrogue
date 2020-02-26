e13a60441861cc9d896d65efb0875069b6c1bf69
Zeno Rogue <zeno@attnam.com>
2020-02-26 01:46:43 +0100

    emscripten uses ZLIB

--- embuild.sh
+++ embuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/lib/emscripten/em++ hyperweb.cpp -O3 -s LEGACY_GL_EMULATION=1 -s TOTAL_MEMORY=128*1024*1024 -std=c++11 -o hyper.html
+/usr/lib/emscripten/em++ hyperweb.cpp -O3 -s USE_ZLIB=1 -s LEGACY_GL_EMULATION=1 -s TOTAL_MEMORY=128*1024*1024 -std=c++11 -o hyper.html
 
