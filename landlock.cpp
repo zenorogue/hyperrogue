6447333d03c89db3384689a8ee1ffad5b9861bd6
Zeno Rogue <zeno@attnam.com>
2020-03-12 10:33:03 +0100

    Frog Park is no longer generated adjacent to Land of Motion

--- landlock.cpp
+++ landlock.cpp
@@ -187,6 +187,7 @@ EX bool incompatible1(eLand l1, eLand l2) {
   if(l1 == laWarpSea && l2 == laKraken) return true;
   if(l1 == laPrairie && l2 == laCrossroads3) return true;
   if(l1 == laWet && l2 == laDesert) return true;
+  if(l1 == laFrog && l2 == laMotion) return true;
   if(isElemental(l1) && isElemental(l2)) return true;
   return false;
   }
