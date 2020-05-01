c9650cda2c4437553f805cf02a106a01319b2efb
Zeno Rogue <zeno@attnam.com>
2020-05-01 10:46:38 +0200

    crystal:: changed condition from cheater to allowIncreasedSight

--- crystal.cpp
+++ crystal.cpp
@@ -822,12 +822,12 @@ EX bool crystal_cell(cell *c, transmatrix V) {
 
   if(!cryst) return false;
 
-  if(view_east && cheater) {
+  if(view_east && allowIncreasedSight()) {
     int d = dist_alt(c);
     queuestr(V, 0.3, its(d), 0xFFFFFF, 1);
     }
 
-  if(view_coordinates && cheater && WDIM == 2) {
+  if(view_coordinates && WDIM == 2 && allowIncreasedSight()) {
     
     auto m = crystal_map();
     
