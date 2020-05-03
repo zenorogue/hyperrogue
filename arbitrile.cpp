f56e0fe2e3ef2dc315f51b7fa58722e99396ceb6
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2020-05-03 15:11:49 -0400

    Silence an unused-lambda-capture warning.

--- arbitrile.cpp
+++ arbitrile.cpp
@@ -380,7 +380,7 @@ void connection_debugger() {
     string cap = its(k) + primes(last.second) + " -> " + its(get<0>(con)) + primes(get<1>(con)) + (get<2>(con) ? " (m) " : "");
     dialog::addSelItem(cap, "go", '0' + k);
     
-    dialog::add_action([k, last, &sh, con] {
+    dialog::add_action([k, last, con] {
       debug_polys.emplace_back(last.first * get_adj(debugged, last.second, k, -1), get<0>(con));
       });
     
