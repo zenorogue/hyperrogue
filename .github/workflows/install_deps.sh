dd31c84d486673e0263715a5e36f94c4adf42262
still-flow <46608177+still-flow@users.noreply.github.com>
2020-10-01 03:30:41 +0300

    new round of CI improvements/fixes
    for Github CI:
    - exit->quit in gdb cmdlist
    - remove unnecessary backtrace gdb cmd
    - remove mymake output buffering for all platforms
    - remove `brew update` step
    - move from to-be-deprecated emscripten container
    
    also update Travis config in sync:
    - try and skip `brew update`

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -2,7 +2,7 @@ set -x -e o pipefail
 
 
 GH_DEPS_UBUNTU="$GH_COMPILER libsdl1.2-dev libsdl-ttf2.0-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev"
-GH_DEPS_MACOS="sdl sdl_gfx sdl_mixer sdl_ttf"
+GH_DEPS_MACOS="sdl sdl_gfx sdl_mixer sdl_ttf" # macos-latest already has both gcc and clang
 GH_DEPS_MINGW64="$GH_COMPILER:x SDL:x SDL_ttf:x SDL_gfx:x SDL_mixer:x make gdb:x"
 
 if [[ "$GH_HYP_GLEW" == "glew_1" ]]; then
@@ -26,8 +26,8 @@ fi
 if [[ "$GH_OS" == "ubuntu-latest" ]]; then
   sudo apt-get -y install $GH_DEPS_UBUNTU
 elif [[ "$GH_OS" == "macos-latest" ]]; then
-  brew update
-  # macos-latest already has both gcc and clang
+  # As of 2020-09-30, brew update sometimes times out on Github CI, so we don't do it here
+  # it's also arguably not necessary because VM images already contain recent-ish homebrew database
   brew install $GH_DEPS_MACOS
   # work around https://stackoverflow.com/questions/51034399/ for now
   (cd /usr/local/include && ln -sf SDL/SDL.h)
