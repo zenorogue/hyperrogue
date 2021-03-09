1a43e1330f5d9630365589471cb6de8368ac31cf
Zeno Rogue <zeno@attnam.com>
2021-03-09 16:19:02 +0100

    missing override

--- config.cpp
+++ config.cpp
@@ -176,7 +176,7 @@ struct bool_setting : public setting {
   virtual bool affects(void *v) override { return v == value; }
   virtual void show_edit_option(char key) override;
   virtual cld get_cld() override { return *value ? 1 : 0; }
-  virtual void load_from(const string& s) {
+  virtual void load_from(const string& s) override {
     *value = parseint(s);
     }
   };
