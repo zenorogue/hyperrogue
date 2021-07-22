db9e9f879c3946384f151d00b0a694df6d8f2ee4
blargdag <blargdag@quickfur.ath.cx>
2021-07-22 08:06:03 -0700

    Fix typo in Polish translation.
    Wrong key was used for deep water.

--- language-pl.cpp
+++ language-pl.cpp
@@ -8399,7 +8399,7 @@ N("shallow water", GEN_F, "płytka woda", "płytkie wody", "płytką wodę", "p�
 S("Shallow water is passable both for land creatures and for water creatures.",
   "Przez płytką wodę mogą przejść i stworzenia lądowe, i wodne.")
 
-N("shallow water", GEN_F, "głęboka woda", "głębokie wody", "głęboką wodę", "głęboką wodą")
+N("deep water", GEN_F, "głęboka woda", "głębokie wody", "głęboką wodę", "głęboką wodą")
 
 S("This body of water is calm and beautiful, but too deep to pass.",
   "Ta woda jest spokojna i piękna, ale zbyt głęboka, by przez nią przejść.")
