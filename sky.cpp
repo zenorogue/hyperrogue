585ab12e4fa254cfd2d84fa306b90b86e219ed2d
Zeno Rogue <zeno@attnam.com>
2022-12-15 19:20:56 +0100

    reverted sky fixup

--- sky.cpp
+++ sky.cpp
@@ -71,7 +71,7 @@ void compute_skyvertices(const vector<sky_item>& sky) {
   if(!draw_sky) return;
   if(vid.wall_height < 0 && geom3::euc_in_hyp()) return; /* just looks bad, hollow horospheres should not have sky */
   if(vid.wall_height < 0 && meuclid && geom3::ggclass() == gcNIH) return; /* same */
-  if(mhyperbolic && among(geom3::ggclass(), gcSol, gcSolN)) return;  /* errors */
+  if(among(geom3::ggclass(), gcSol, gcSolN)) return;  /* errors */
 
   int sk = get_skybrightness();
   
