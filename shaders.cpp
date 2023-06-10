aa729c912c9738c69ace910d11b2cd1fecf247da
Zeno Rogue <zeno@attnam.com>
2023-06-10 10:35:09 +0200

    fixed stereo_alpha in some geometries

--- shaders.cpp
+++ shaders.cpp
@@ -520,7 +520,9 @@ shared_ptr<glhr::GLprogram> write_shader(flagtype shader_flags) {
       shader_flags |= SF_ORIENT;
       }
     else if((shader_flags & SF_PERS3) && stereo_alpha && !vrhr::rendering_eye()) {
+      vmain += "t = uPP * t;", vsh += "uniform mediump mat4 uPP;";
       vmain += stereo_shader();
+      shader_flags |= SF_ORIENT;
       }
       
     vmain += "gl_Position = uP * t;\n";
