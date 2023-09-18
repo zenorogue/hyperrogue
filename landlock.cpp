66ea0804cf215c1e5c4e62f8f06bf2f3403c28d8
Zeno Rogue <zeno@attnam.com>
2023-09-18 11:10:41 +0200

    ls:: more careful incompatibility handling in voronoi

--- landlock.cpp
+++ landlock.cpp
@@ -592,7 +592,7 @@ EX eLand getNewLand(eLand old, eLand old2 IS(laBarrier)) {
     }
   
   eLand n = old;
-  while(incompatible(n, old) || !isLandIngame(n)) {
+  while(incompatible(n, old) || incompatible(n, old2) || !isLandIngame(n)) {
     n = tab[hrand(cnt)];
     if(weirdhyperbolic && specialland == laCrossroads4 && isCrossroads(n))
       n = laCrossroads4;
