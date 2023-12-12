24c196db1598d4e1b1fc9ab7ac7ffb93257a51bc
Zeno Rogue <zeno@attnam.com>
2023-12-12 20:02:53 +0100

    limit setland errors

--- barriers.cpp
+++ barriers.cpp
@@ -324,12 +324,15 @@ EX void setbarrier(cell *c) {
   setbarrier(c, c->barleft, c->barright, ctof(c));
   }
 
+EX int setland_max = 5;
+
 EX void setland(cell *c, eLand l) {
   if(c->land != l)  {
     c->landparam = 0;  
     }
-  if(l == laNone) {
-    printf("setland\n"); // NONEDEBUG
+  if(l == laNone && setland_max > 0) {
+    setland_max--;
+    printf("error: set land to laNone\n"); // NONEDEBUG
     }
   c->land = l;
   }
