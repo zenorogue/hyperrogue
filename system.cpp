723422e13797d53aca406e53cc27c7edf7c81b80
Zeno Rogue <zeno@attnam.com>
2024-05-08 21:32:26 +0200

    disable custom_land_list in princess

--- system.cpp
+++ system.cpp
@@ -1630,6 +1630,7 @@ EX void switch_game_mode(char switchWhat) {
       land_structure = princess::challenge ? lsSingle : lsNiceWalls;
       inv::on = false;
       racing::on = false;
+      use_custom_land_list = false;
       dual::disable();
       break;
     
