441f825566fb2a33bd3c4e8bead8bee1b0aa743d
Zeno Rogue <zeno@attnam.com>
2024-05-25 11:45:47 +0200

    export achievement data

--- achievement.cpp
+++ achievement.cpp
@@ -119,8 +119,8 @@ EX int score_default(int id);
 EX void improveItemScores();
 EX void upload_score(int id, int v);
 
-string achievementMessage[3];
-int achievementTimer;
+EX string achievementMessage[3];
+EX int achievementTimer;
 /** achievements received this game */
 EX vector<string> achievementsReceived;
 
