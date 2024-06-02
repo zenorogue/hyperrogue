6a086e9764e2eb16c855878efa488f2f831690f3
Zeno Rogue <zeno@attnam.com>
2024-06-02 18:17:59 +0200

    fixup to header

--- hyper.h
+++ hyper.h
@@ -800,7 +800,7 @@ string XLAT(string);
 #define DKEY (get_direction_key(sym, uni))
 #define DIRECTIONKEY (interpret_as_direction(sym, uni) ? uni : 0)
 
-namespace scores { void load(); }
+namespace scores { void load(); void load_only(); }
 
 #if ISMOBILE
 namespace leader { void showMenu(); void handleKey(int sym, int uni); }
