b75c43f7a4e88b79aa77e8ef7f3fdd3f0e7c721d
Zeno Rogue <zeno@attnam.com>
2024-06-21 15:01:09 +0200

    added randf_from

--- game.cpp
+++ game.cpp
@@ -62,9 +62,11 @@ EX vector<int> hrandom_permutation(int qty) {
 /** Use \link hrngen \endlink to generate a floating point number between 0 and 1.
  */
 
-EX ld hrandf() { 
-  return (hrngen() - hrngen.min()) / (hrngen.max() + 1.0 - hrngen.min());
+EX ld randf_from(std::mt19937& r) { 
+  return (r() - r.min()) / (r.max() + 1.0 - r.min());
   }
+
+EX ld hrandf() { return randf_from(hrngen); }
 
 /** Returns an integer corresponding to the current state of \link hrngen \endlink.
  */
