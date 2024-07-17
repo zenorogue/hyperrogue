e881812fa45a5098855e5d76ad7a24988e9f2d3c
Zeno Rogue <zeno@attnam.com>
2024-07-17 11:18:37 +0200

    fixed a bug with binary_width not set

--- config.cpp
+++ config.cpp
@@ -1212,6 +1212,7 @@ EX void initConfig() {
   -> editable("flashing effects", 'h')
   -> help("Disable if you are photosensitive. Replaces flashing effects such as Orb of Storms lightning with slow, adjustable animations.");
 
+  vid.binary_width = 1;
   param_custom_ld(vid.binary_width, "binary tiling width", menuitem_binary_width, 'v');
 
   param_b(fake::multiple_special_draw, "fake_multiple", true);
