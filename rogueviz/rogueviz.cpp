94064a18af2d5d63afeb08d0d6bda196b5654288
Zeno Rogue <zeno@attnam.com>
2024-07-21 21:30:50 +0200

    rogueviz:: draw better lines in solv/sl2 visualizations

--- rogueviz/rogueviz.cpp
+++ rogueviz/rogueviz.cpp
@@ -621,11 +621,16 @@ bool drawVertex(const shiftmatrix &V, cell *c, shmup::monster *m) {
       
       if(callhandlers(false, hooks_alt_edges, ei, false)) ;
 
-      else if(pmodel && !fat_edges) {
+      else if(sl2)
+        rots::queueline_correct(h1, h2, col, 2 + vid.linequality, PPR::STRUCT0);
+
+      else if(sol && !fat_edges)
+        sn::queueline_lie(h1, h2, col, 2 + vid.linequality, PPR::STRUCT0);
+
+      else if(pmodel && !fat_edges && !sol) {
         queueline(h1, h2, col, 2 + vid.linequality).prio = PPR::STRUCT0;
         }
       else {
-      
         cell *center = multidraw ? c : centerover;
       
         if(!multidraw && ei->orig && ei->orig != center && celldistance(ei->orig, center) > 3) 
