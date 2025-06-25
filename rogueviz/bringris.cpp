e0f2e86fed029b0bb6d59461486aea64954ad8af
Zeno Rogue <zeno@attnam.com>
2025-06-25 22:02:58 +0200

    rogueviz:: bringris:: fixed the typo which caused the score not to be sent to the leaderboards

--- rogueviz/bringris.cpp
+++ rogueviz/bringris.cpp
@@ -752,7 +752,7 @@ void new_piece() {
   if(shape_conflict(at)) {
     playSound(cwt.at, "die-bomberbird");
     state = tsGameover;
-    #if RCVOL
+    #if RVCOL
     if(cur.pro_game && cur.max_piece == bgeoms[bgeom].default_max_piece)
       rogueviz::rv_leaderboard(bgeoms[bgeom].name, cur.score, 1, rvlc::num, lalign(0, cur.bricks, " ", cur.cubes, " ", cur.completed));
     #endif
